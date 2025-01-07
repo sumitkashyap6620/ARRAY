758cc21820c27a19b078dc907889e33b538aa764
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-01-07 23:03:56 +0530

    Binary To Decimal Conversion

---
+++ BinaryToDecimal.java
@@ -0,0 +1,23 @@
+package ARRAY;
+import java.util.Scanner;
+public class BinaryToDecimal {
+    public static void IntoDecimal(int[] arr){
+        int a = 0 ;
+        int d = 0 ; 
+        for(int i = 0 ; i < arr.length ; i++){
+           d = d + (int)(Math.pow(2,a)*arr[i]);
+          a++ ;
+        }
+        System.out.print("Decimal No. = "+d);
+    }
+    public static void main(String[] args) {
+      
+            System.out.print(" GIVE THE SIZE OF BINARY DIGITS TO CONVERT INTO DECIMAL : ");
+             Scanner sc = new Scanner (System.in);
+             int[] arr = new int[sc.nextInt()];
+             System.out.println("GIVE THE DIGITS :");
+             for (int i = 0 ; i < arr.length ; i++)arr[i] = sc.nextInt();
+             IntoDecimal(arr);
+             sc.close();
+        }
+    }
