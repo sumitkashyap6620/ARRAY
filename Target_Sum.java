9326d7d35c744f957efde3f72d3714815b8c29fa
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-01-09 11:19:03 +0530

    If targer is not present in array

--- Target_Sum.java
+++ Target_Sum.java
@@ -2,12 +2,15 @@ package ARRAY;
 import java.util.Scanner ;
 public class Target_Sum {
     public static void targetsum (int[] arr , int t){
-     for(int i = 0 ; i < arr.length ; i++){
+     int var = 0 ;
+        for(int i = 0 ; i < arr.length ; i++){
         for (int j = i+1 ; j < arr.length ; j++)
         if (arr[i] + arr[j] == t){
             System.out.println(arr[i]+" , "+arr[j]+"   At indexes ["+i+"] "+"["+j+"]");
+          var  ++ ;
         }
      }
+     if(var == 0 )System.out.print("Target not Present in Array ");
     }
     public static void main(String[] args) {
         System.out.print("Give size of array : ");
