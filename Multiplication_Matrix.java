d28e4510b132f427f087f290db53960c51e9be67
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-01-10 23:10:46 +0530

    add Some Funcnality

--- Multiplication_Matrix.java
+++ Multiplication_Matrix.java
@@ -12,15 +12,11 @@ public class Multiplication_Matrix {
 }
 public static int[][] Multiplication(int[][]arr,int[][]arr2){
 int[][] ans= new int[arr.length][arr2[0].length];
-for (int i = 0 ; i < ans.length ; i++){
-    for (int j = 0 ; j < ans[i].length ; j++){
+for (int i = 0 ; i < ans.length ; i++)
+    for (int j = 0 ; j < ans[i].length ; j++)
       for(int k = 0 ; k < arr2.length ; k++)
-      {
         ans [i][j] += arr[i][k] * arr2[k][j] ;
-    
-      }
-    }
-}
+
 return ans ;
 }
   
@@ -38,10 +34,17 @@ return ans ;
    int r2 = sc.nextInt();
     System.out.print("columns :- ");
     int c2 = sc.nextInt();
+    if(c != r2){
+    System.out.println("Multiplication is not possible !");
+   return ;
+   
+   }
     int[][] arr2 = new int [r2][c2];
    System.out.println("Give element of 2nd Matrix :");
     for(int i = 0 ; i < r2 ; i++ )
-    for(int j = 0 ; j < c2 ; j++ ) arr2[i][j] = sc.nextInt(); 
+    for(int j = 0 ; j < c2 ; j++ ) 
+    arr2[i][j] = sc.nextInt(); 
+    System.out.println("Multiplication of tow matrix is :- ");
    Display_matrix(Multiplication(arr, arr2));
    sc.close();
 }
