5cb352eb0c3a79422aaf7247df993e5dfb8f6eba
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-05-02 12:34:26 +0530

    programe added

--- AllSubsets.java
+++ AllSubsets.java
@@ -1,9 +1,28 @@
 import java.util.Scanner;
 public class AllSubsets{
+  public static void PrintSubsets(int arr[]){
+   for (int i = 0 ; i < arr.length ; i++){
+    System.out.print("{"+arr[i]+"} ");
+   }
+   System.out.println();
+   for( int i = 0 ; i < arr.length ; i++ ){
+    
+    for (int j = i ; j < arr.length ; j++ ){
+      System.out.print("{");
+    for (int k = j ; k < arr.length ; k++ )
+    System.out.print(arr[j]);
+    }
+    System.out.println("} ");
+   }
+   System.out.println();
+  }
     public static void main (String args[]){
-      System.out.print("give length of array :"); 
+      System.out.print("Give length of array :"); 
       Scanner sc = new Scanner(System.in); 
       int arr[] = new int[sc.nextInt()];
-      System.out.println();
+      System.out.println("Give Elements");
+      for(int i = 0 ; i < arr.length ; i++)
+       arr[i] = sc.nextInt();
+      PrintSubsets(arr);
     }
 }
