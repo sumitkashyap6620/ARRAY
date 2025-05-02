54d1be46c51b2b018275f9391fc63a76a2c39c46
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-05-02 10:39:33 +0530

    logical error solved

--- TrapimngRainWater.java
+++ TrapimngRainWater.java
@@ -7,22 +7,20 @@ public class TrapimngRainWater {
       for(int i = 1 ; i < arr.length-1 ; i++){
         for(int j = 0 ; j < i ; j++){
            if (j==0) max = arr[j] ;
-           else{
+           else
             if(arr[j] > max  ) max = arr[j] ;
-           } 
-        }
+           }
         for(int j = arr.length-1 ; j > i  ; j--){
-            if (j == arr.length) max2 = arr[j] ;
-            else{
+            if (j == arr.length-1) max2 = arr[j] ;
+            else
              if(arr[j] > max2  ) max2 = arr[j] ;
-            } 
          }
-         if(max <= max2 && arr[i] < max ){
+         if(max <= max2 && arr[i] < max )
             trapedwater = trapedwater + max-arr[i] ;
-         }
-         if(max2<=max && arr[i] < max2 ){
+         
+         else if(max2<=max && arr[i] < max2 )
             trapedwater = trapedwater + max2-arr[i] ;
-         }
+         
       }
       return trapedwater ;
     }
@@ -31,10 +29,9 @@ public class TrapimngRainWater {
       Scanner sc = new Scanner(System.in);
      int arr[]= new int[sc.nextInt()]; 
      System.out.println("Give all elements :") ;
-     for(int i = 0 ; i < arr.length ; i++ ){
+     for(int i = 0 ; i < arr.length ; i++ )
           arr[i] = sc.nextInt() ;
-     }
      System.out.println(TrapedWater(arr)) ;
-
+     sc.close();
     }
 }
