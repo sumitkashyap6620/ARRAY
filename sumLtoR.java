598019d648b0b37a632caf8732b4145e3fe007a2
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-06-10 23:01:32 +0530

    completed

--- sumLtoR.java
+++ sumLtoR.java
@@ -3,23 +3,24 @@ public class sumLtoR {
     public static void displayarray(int arr[]){
     for (int i = 0 ; i<arr.length ; i++)System.out.print(arr[i]+" ");
     }
-    public static void prefix(int arr[]){
+    public static int[] prefix(int arr[],int ans[]){
+         ans[1]=arr[1];
+      
         for(int i = 2 ; i<arr.length ; i++)
-        arr[i] = arr[i] + arr[i-1];
-
+        ans[i] = ans[i-1]+arr[i];
+        return ans;
     }
-
-    
-    public static int Lt0R(int arr[],int l , int r){
-     prefix(arr);
-
+     public static int Lt0R(int arr[],int l , int r){
+       int ans[] = new int[arr.length+1];
+       ans = prefix(arr,ans);
+       return ans[r]-ans[l-1];
     }
     public static void main(String[] args) {
         System.out.print("give length of array :");
         Scanner sc = new Scanner(System.in);
-        int arr[] = new int[sc.nextInt()];
+        int arr[] = new int[sc.nextInt()+1];
         System.out.println("give elements:");
-        for(int i = 1 ; i <= arr.length ; i++)
+        for(int i = 1 ; i < arr.length ; i++)
         arr[i]=sc.nextInt();
         int x=0;
         do{
@@ -28,7 +29,7 @@ public class sumLtoR {
             int L = sc.nextInt();
             System.out.print("R = ");
             int R = sc.nextInt();
-           System.out.println(Lt0R(arr,L,R));
+           System.out.println("sum in given range is :- "+Lt0R(arr,L,R));
         System.out.print("to find other sum in range press 1 : ");
          x = sc.nextInt();
         }while(x==1);
