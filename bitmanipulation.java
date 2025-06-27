c4cc2401806f7e98a7f7eda53071773ea74f1662
sumit kashyap <sumit.kashyap.6620@gmail.com>
2025-06-27 10:52:22 +0530

    clear and set bit

--- bitmanipulation.java
+++ bitmanipulation.java
@@ -2,15 +2,25 @@ import java.util.Scanner;
 public class bitmanipulation{
      public static void main (String args[]){
        Scanner sc = new Scanner(System.in);
-          System.out.print("give any number to find bit at any position :-");
+          System.out.print("give any number to get,set,clear bit at any position :-");
           int n = sc.nextInt();
           System.out.print("give position :-");
           int p = sc.nextInt();
-        int bitmas = 1 << p ;
-        if((n & bitmas) == 0)
-          System.out.println("Bit was 0 ");
-         else
-          System.out.println("Bit was 1 ");
-        sc.close();
+         int bitmas = 1 << p ;
+        //  get bit ........................................
+        
+        // if((n & bitmas) == 0)
+        //   System.out.println("Bit was 0 ");
+        //  else
+        //   System.out.println("Bit was 1 ");
+        // sc.close();
+
+        // setbit ...........................................
+        
+        // System.out.println((bitmas | n));
+        
+        // clear bit ........................................
+        bitmas = ~bitmas ;
+        System.out.println(bitmas & n);
       }
 }
